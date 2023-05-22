5801c5251c34aa5a27634bde6ef6141767c9c641
seonsy44 <seonsy44@gamil.com>
2023-05-22 16:35:59 +0900

    Button 생성자 함수 작성

---
+++ client/src/Button.js
@@ -0,0 +1,23 @@
+export function Button(name, onClick) {
+  this.name = name;
+
+  this.buttonEl = document.createAttribute("button");
+  this.buttonEl.textContent = name;
+
+  var that = this;
+  this.buttonEl.onclick = function () {
+    onClick(that);
+  };
+}
+
+Button.prototype.render = function (parentEl) {
+  parentEl.append(this.buttonEl);
+};
+
+Button.prototype.disable = function (isDisable) {
+  if (isDisable) {
+    this.buttonEl.setAttribute("disabled", true);
+  } else {
+    this.buttonEl.removeAttribute("disabled");
+  }
+};
